2818d493609bbe7a6a1b9e05f873081a691e2a01
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2018-03-11 00:36:25 +0530

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/SurejaHit/Career_guidance_app"""
    This reverts commit dc89306a686af81f1bc492f5dac6f306c2d71871.

--- app/src/main/java/com/example/surejahit/career_guidance_app/Guidance_Fragment.java
+++ app/src/main/java/com/example/surejahit/career_guidance_app/Guidance_Fragment.java
@@ -1,55 +1,30 @@
 package com.example.surejahit.career_guidance_app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
-public class Guidance_Fragment extends AppCompatActivity{
+
+
+public class Guidance_Fragment extends Fragment{
+
     public Guidance_Fragment() {
         // Required empty public constructor
     }
-    private Button btn10=findViewById(R.id.btn10);
-    private Button btn12=findViewById(R.id.btn12);
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Onclick10();
-        onclick12();
     }
 
-   /*@Override
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_guidance, container, false);
-
-    }*/
-    public void Onclick10()
-    {
-
-
-        btn10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("com.example.surejahit.career_guidance_app.CareerAfter10");
-                startActivity(intent);
-            }
-        });
     }
-    public void onclick12()
-    {
-        btn12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("com.example.surejahit.career_guidance_app.CareerAfter12th");
-                startActivity(intent);
-            }
-        });
-    }
+
 }
