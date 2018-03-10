7559733f1c13bcaae72ca5d318596905566cc872
Dharmik98 <31448372+Dharmik98@users.noreply.github.com>
2018-03-10 18:29:19 +0530

    New changes in Login Activity

--- app/src/main/java/com/example/surejahit/career_guidance_app/SplashActivity.java
+++ app/src/main/java/com/example/surejahit/career_guidance_app/SplashActivity.java
@@ -15,7 +15,6 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                
                 Intent i = new Intent(SplashActivity.this, Language_Activity.class);
                 startActivity(i);
                 finish();
