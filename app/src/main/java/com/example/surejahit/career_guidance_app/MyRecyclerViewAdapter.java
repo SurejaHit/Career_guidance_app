df500e05aa4004158231c5c85f19827c097ffc08
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2018-03-11 04:19:54 +0530

    List view added dor future trends

---
+++ app/src/main/java/com/example/surejahit/career_guidance_app/MyRecyclerViewAdapter.java
@@ -0,0 +1,84 @@
+package com.example.surejahit.career_guidance_app;
+
+/**
+ * Created by Mit Patel on 11-Mar-18.
+ */
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class MyRecyclerViewAdapter extends RecyclerView
+        .Adapter<MyRecyclerViewAdapter
+        .ddDataObjectHolder> {
+    private static String LOG_TAG = "MyRecyclerViewAdapter";
+    private ArrayList<DataObject> mDataset;
+    private static MyClickListener myClickListener;
+
+    public static class ddDataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView label;
+        TextView dateTime;
+
+        public ddDataObjectHolder(View itemView) {
+            super(itemView);
+            label = (TextView) itemView.findViewById(R.id.textView);
+            dateTime = (TextView) itemView.findViewById(R.id.textView2);
+            Log.i(LOG_TAG, "Adding Listener");
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+
+    public MyRecyclerViewAdapter(ArrayList<DataObject> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public ddDataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_row, parent, false);
+
+        ddDataObjectHolder dataObjectHolder = new ddDataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ddDataObjectHolder holder, int position) {
+        holder.label.setText(mDataset.get(position).getmText1());
+        holder.dateTime.setText(mDataset.get(position).getmText2());
+    }
+
+    public void addItem(DataObject dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
