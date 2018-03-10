2187ae49ff0c23b37bc8011fea3e8c363eb60757
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2018-03-10 12:48:49 +0530

    Login Activity Added

---
+++ app/src/main/java/com/example/surejahit/career_guidance_app/Login_Activity.java
@@ -0,0 +1,8 @@
+package com.example.surejahit.career_guidance_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+}
