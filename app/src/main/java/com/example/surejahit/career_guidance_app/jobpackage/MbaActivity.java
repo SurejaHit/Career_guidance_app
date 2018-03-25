08311dc9896e781ed4285a3a398228480748fcbb
Dharmik98 <31448372+Dharmik98@users.noreply.github.com>
2018-03-25 10:11:43 +0530

    new updates by dharmik at 10:11 am

--- app/src/main/java/com/example/surejahit/career_guidance_app/jobpackage/MbaActivity.java
+++ app/src/main/java/com/example/surejahit/career_guidance_app/jobpackage/MbaActivity.java
@@ -2,6 +2,9 @@ package com.example.surejahit.career_guidance_app.jobpackage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 import com.example.surejahit.career_guidance_app.R;
 
@@ -11,5 +14,42 @@ public class MbaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mba);
+        WebView webview1 = (WebView) findViewById(R.id.webVideoView1mba);
+
+        webview1.setVisibility(View.VISIBLE);
+        String html1 = " <iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/_kHGOENffoQ\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>";
+
+
+        webview1.getSettings().setJavaScriptEnabled(true);
+        webview1.setWebChromeClient(new WebChromeClient() {
+        });
+
+        webview1.loadData(html1, "text/html", "utf-8");
+
+        WebView webview2 = (WebView) findViewById(R.id.webVideoView2mba);
+
+        webview2.setVisibility(View.VISIBLE);
+        String html2 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/0C4QBIxsCiI\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>";
+
+
+        webview2.getSettings().setJavaScriptEnabled(true);
+        webview2.setWebChromeClient(new WebChromeClient() {
+        });
+
+        webview2.loadData(html2, "text/html", "utf-8");
+
+
+        WebView webview3 = (WebView) findViewById(R.id.webVideoView3mba);
+
+        webview3.setVisibility(View.VISIBLE);
+        String html3 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/YFf2vWDkVkY\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>";
+
+
+        webview3.getSettings().setJavaScriptEnabled(true);
+        webview3.setWebChromeClient(new WebChromeClient() {
+        });
+
+        webview3.loadData(html3, "text/html", "utf-8");
+
     }
 }
