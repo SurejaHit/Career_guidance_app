cd24e93d6229e2e521ea1295c3ed3400dffe2927
AGENT-007\Mit Patel <mitpatel23897@gmail.com>
2018-03-25 09:51:20 +0530

    Graduation added

--- app/src/main/java/com/example/surejahit/career_guidance_app/CareerAfter_g.java
+++ app/src/main/java/com/example/surejahit/career_guidance_app/CareerAfter_g.java
@@ -1,9 +1,13 @@
 package com.example.surejahit.career_guidance_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.surejahit.career_guidance_app.R;
+import com.example.surejahit.career_guidance_app.jobpackage.MbaActivity;
 
 /**
  * Created by dwije on 23-03-18.
@@ -11,9 +15,32 @@ import com.example.surejahit.career_guidance_app.R;
 
 public class CareerAfter_g extends Activity
 {
+    private Button btn1;
+    private Button btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_after_g);
+
+        btn1 = (Button) findViewById(R.id.btn11);
+        btn2 = findViewById(R.id.btn22);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),StartupActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MbaActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/surejahit/career_guidance_app/CareerAfter_pg.java
+++ app/src/main/java/com/example/surejahit/career_guidance_app/CareerAfter_pg.java
@@ -1,7 +1,10 @@
 package com.example.surejahit.career_guidance_app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.surejahit.career_guidance_app.R;
 
@@ -9,11 +12,40 @@ import com.example.surejahit.career_guidance_app.R;
  * Created by dwije on 23-03-18.
  */
 
+
 public class CareerAfter_pg extends Activity
 {
+    private Button btn3;
+    //private Button btn4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_after_pg);
+
+        btn3 = findViewById(R.id.btn33);
+        // btn4 = findViewById(R.id.btn44);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),StartupActivity.class);
+                startActivity(i);
+            }
+        });
+
+        /*btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),.class);
+                startActivity(i);
+
+            }
+        });
+
+        */
+
+
+
+
     }
 }
+
